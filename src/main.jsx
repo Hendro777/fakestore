94b33464c357e09ad458210dbc4308f11fbecd3d
infwgj697 <hendrik.lind@haw-hamburg.de>
2023-10-03 15:09:52 +0200

    feat: added simple error page
    I created this branch form the same commit i created the notfound page on.
    With this in mind I wanted to get into proper merging! :D

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import '@fontsource-variable/karla'
 
 import SiteLayout from "./components/SiteLayout"
 import Home from './pages/Home'
+import Error from "./components/Error"
 import Categories, { loader as categoriesLoader } from './pages/Categories'
 import Products, { loader as productsLoader } from './pages/Products'
 import ProductDetails, { loader as productDetailsLoader } from './pages/ProductDetails/ProductDetails'
@@ -19,7 +20,7 @@ const router = createBrowserRouter(createRoutesFromElements(
   <Route element={<SiteLayout />}>
     <Route index element={<Home />} />
     <Route path="categories" element={<Categories />} loader={categoriesLoader} />
-    <Route path="products" element={<Products />} loader={productsLoader} />
+    <Route path="products" element={<Products />} errorElement={<Error />} loader={productsLoader} />
     <Route path="products/:id" element={<ProductDetails />} loader={productDetailsLoader}>
       <Route index element={<ProductDescription />} />
       <Route path="specifications" element={<ProductSpecifications />} />
