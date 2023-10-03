445bf46953bd9a11a5f7c26f244aafecd107928f
infwgj697 <hendrik.lind@haw-hamburg.de>
2023-10-03 15:59:04 +0200

    style: styled NotFound Page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,10 @@
 export default function NotFound() {
-    return (
-      <div className="vh-container notFound">
+  return (
+    <div className="vh-container notFound">
+      <div className="container">
         <h1>404 - Not Found</h1>
         <p>The page you are looking for does not exist.</p>
       </div>
-    );
-  }
+    </div>
+  );
+}
