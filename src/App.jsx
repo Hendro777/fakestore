348c0dc6d2d91c9a22d72cb2ce0ba04e6a9557b2
infwgj697 <hendrik.lind@haw-hamburg.de>
2023-10-04 15:02:14 +0200

    refactor: Moved routing to App.jsx, changed build script

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
-const App = function() {
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+
+import SiteLayout from "./components/SiteLayout"
+import Home from './pages/Home'
+import Error from "./components/Error"
+import Categories, { loader as categoriesLoader } from './pages/Categories'
+import Products, { loader as productsLoader } from './pages/Products'
+import ProductDetails, { loader as productDetailsLoader } from './pages/ProductDetails/ProductDetails'
+import ProductDescription from "./pages/ProductDetails/ProductDescription"
+import ProductSpecifications from './pages/ProductDetails/ProductSpecifications'
+import ProductReviews from './pages/ProductDetails/ProductReviews'
+import NotFound from './pages/NotFound'
+import Login, { loader as loginLoader, action as loginAction } from './pages/Login'
+import Account, { loader as accountLoader } from './pages/Account/Account'
+import { requireAuth } from './utils/auth'
+
+export default function App() {
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route element={<SiteLayout />}>
+      <Route index element={<Home />} />
+      <Route path="login" element={<Login />} loader={loginLoader} action={loginAction} />
+      <Route path="account" element={<Account />} loader={accountLoader} />
+      <Route path="categories" element={<Categories />} loader={categoriesLoader} />
+      <Route path="products" element={<Products />} errorElement={<Error />} loader={productsLoader} />
+      <Route path="products/:id" element={<ProductDetails />} loader={productDetailsLoader}>
+        <Route index element={<ProductDescription />} />
+        <Route path="specifications" element={<ProductSpecifications />} />
+        <Route path="reviews" element={<ProductReviews />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  ), {
+    basename: import.meta.env.DEV ? '/' : '/fakestore/'
+  })
+
   return (
-    <></>
+    <RouterProvider router={router} />
   )
 }
-
-export default App
